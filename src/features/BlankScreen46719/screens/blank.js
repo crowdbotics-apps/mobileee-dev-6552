6b2ed683bee3f6503716a735a86536da3c776c67
Crowdbotics <team@crowdbotics.com>
2020-06-25 11:32:17 +0000

    Update components

--- src/features/BlankScreen46719/screens/blank.js
+++ src/features/BlankScreen46719/screens/blank.js
@@ -67,6 +67,37 @@ export class _Blank extends React.Component {
     >
       <Image
         resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/Hydrangeas_iwX6JrM.jpg"
+        }}
+        style={{
+          width: 99,
+          height: 99,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontFamily: "System"
+        }}
+      />
+      <Image
+        resizeMode="cover"
         style={{
           width: 99,
           height: 99,
